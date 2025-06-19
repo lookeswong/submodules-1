59a88a4437e9dc4d806e63c43545c8eda7f6be6b
LooKes Wong <lookes.wong@kingdomdigital.com.my>
2025-06-19 16:17:20 +0800

    update

---
+++ studio-2/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Studio2() {
+    return <div>Studio2 page</div>;
+}
---
+++ studio/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Studio() {
+    return <div>Studio page</div>;
+}
